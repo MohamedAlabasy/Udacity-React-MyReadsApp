f1e0dcca285f67bf7d4dd88d68fce1eb012be065
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-09-12 00:55:05 +0200

    update emptyStates

--- src/pages/emptyStates.jsx
+++ src/pages/emptyStates.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom'
 import empty from '../assets/images/empty.png'
-
+import { FaHome } from "react-icons/fa";
 export default function NotFound() {
     return (
 
@@ -9,7 +9,9 @@ export default function NotFound() {
             <div >
                 <img src={empty} alt="there is no data to show" />
             </div>
-            <Link to={'/'} className='btn btn-dark'>Back to Home</Link>
+            <Link to={"/"} className="btn btn-dark ms-3 " style={{ width: '150px' }}>
+                <FaHome style={{ width: '20px', height: '20px', color: 'white' }} />
+            </Link>
         </div>
 
     )
