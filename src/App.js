4da77f515c374fce228ff1f3183af793765e2882
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-09-11 15:11:36 +0200

    add route

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import logo from './logo.svg';
+
+import Home from './pages/Home'
+import Search from './pages/Search'
+import EmptyStates from './pages/emptyStates'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <div className='btn btn-danger'>hello</div>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' exact element={<Home />} />
+        <Route path='/search' exact element={<Search />} />
+
+        <Route path='*' element={<EmptyStates />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Search() {
     return (
-        <div>Search</div>
+        <>
+            <div className='container text-center align-items-center pb-2'>
+                <div>Search</div>
+                <Link to={'/'} className='btn btn-dark'>Back to Home</Link>
+            </div>
+        </>
     )
 }
